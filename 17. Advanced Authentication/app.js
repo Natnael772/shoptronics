3e559af312cce7b633576d1182c562e4b283ca5d
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-01-04 23:51:29 +0300

    Auth advanced

--- 17. Advanced Authentication/app.js
+++ 17. Advanced Authentication/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-//session
 const session = require("express-session");
 
 const sequelizeStore = require("connect-session-sequelize")(session.Store);
