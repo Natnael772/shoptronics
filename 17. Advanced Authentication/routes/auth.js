288557b26b721bae45265c083ebc77ae73db0b67
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-01-05 16:01:20 +0300

    Adding crypto

--- 17. Advanced Authentication/routes/auth.js
+++ 17. Advanced Authentication/routes/auth.js
@@ -9,4 +9,5 @@ router.post("/logout", authController.postLogout);
 
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
+router.get("/reset", authController.getReset);
 module.exports = router;
